12b0906eeb7ba5205b3dd5a054140d183cf1aaca
cloudnull <kevin.carter@rackspace.com>
2013-06-17 18:20:47 -0500

    PEP8 Formatting

--- bin/tribbleclient.local.py
+++ bin/tribbleclient.local.py
@@ -5,7 +5,9 @@ import sys
 possible_topdir = os.path.normpath(os.path.join(os.path.abspath(sys.argv[0]),
                                                 os.pardir, os.pardir))
 
-if os.path.exists(os.path.join(possible_topdir, 'tribbleclient', '__init__.py')):
+if os.path.exists(os.path.join(possible_topdir,
+                               'tribbleclient',
+                               '__init__.py')):
     sys.path.insert(0, possible_topdir)
 
 from tribbleclient import exectuable
