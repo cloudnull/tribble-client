b3464228a76b336e93ccdbe602b3806269543c90
Kevin Carter <kevin.carter@rackspace.com>
2014-03-02 21:49:06 -0600

    moar updates

--- setup.py
+++ setup.py
@@ -2,20 +2,31 @@
 
 import setuptools
 import sys
+
 from tribbleclient import info
 
-# Check the version of Python that we have been told to use
+
+with open('requirements.txt') as f:
+    REQUIRED = f.read().splitlines()
+
 if sys.version_info < (2, 6, 0):
-    sys.stderr.write('The Tribble System Presently requires'
-                     ' Python 2.6.0 or greater\n')
-    sys.exit('\nUpgrade python because your version of it is '
-             ' VERY deprecated\n')
+    sys.stderr.write("Tribble Presently requires Python 2.6.0 or greater \n")
+    raise SystemExit(
+        '\nUpgrade python because you version of it is VERY deprecated\n'
+    )
 
-with open('README') as r_file:
-    long_description = r_file.read()
+if sys.version_info < (2, 7, 0):
+    REQUIRED.append('argparse')
 
-T_M = ['requests',
-       'prettytable']
+with open('README', 'rb') as r_file:
+    LDINFO = r_file.read()
+
+
+MODULES = [
+    'tribbleclient',
+    'tribbleclient.arguments'
+]
+
 
 setuptools.setup(
     name=info.__appname__,
@@ -23,20 +34,23 @@ setuptools.setup(
     author=info.__author__,
     author_email=info.__email__,
     description=info.__description__,
-    long_description=long_description,
+    long_description=LDINFO,
     license=info.__license__,
-    packages=['tribbleclient'],
+    packages=MODULES,
     url=info.__urlinformation__,
-    install_requires=T_M,
+    install_requires=REQUIRED,
     classifiers=[
         'Intended Audience :: Information Technology',
         'Intended Audience :: System Administrators',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
+        ('License :: OSI Approved :: GNU General Public License v3 or'
+         ' later (GPLv3+)'),
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Topic :: Utilities',
-        'Topic :: Software Development :: Libraries :: Python Modules'],
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
     entry_points={
         "console_scripts": ["tribble = tribbleclient.executable:execute"]
     }
